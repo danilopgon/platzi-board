a6f97008b024279e7e646b17eb2d698470ac45b7
danilopgon <dani.lopezgonzalez.94@gmail.com>
2024-06-13 17:46:05 +0200

    Take until to stop drawing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fromEvent, map, mergeAll } from "rxjs";
+import { fromEvent, map, mergeAll, takeUntil } from "rxjs";
 
 const canvas = document.querySelector("#reactive-canvas");
 
@@ -12,13 +12,15 @@ const updateCursorPosition = (event) => {
 const onMouseDown$ = fromEvent(canvas, "mousedown").pipe(
   map(updateCursorPosition)
 );
-const onMouseMove$ = fromEvent(canvas, "mousemove");
 const onMouseUp$ = fromEvent(canvas, "mouseup");
+const onMouseMove$ = fromEvent(canvas, "mousemove").pipe(takeUntil(onMouseUp$));
 
 onMouseDown$.subscribe();
 
 const canvasContext = canvas.getContext("2d");
 canvasContext.lineWidth = 8;
+canvasContext.lineJoin = "round";
+canvasContext.lineCap = "round";
 canvasContext.strokeStyle = "white";
 
 const paintStroke = (event) => {
